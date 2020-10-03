c8feff7bcb00df81fc9128226999d55c84747d76
Fedo Viktor <fedviktor@bitbucket.org>
2020-10-03 23:39:30 +0300

    fix: src/index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-exports.min = function min([array]) {
-    if (array.length === 0 || array===undefined) {
-        return 0;
-    } 
-    else {
-        let minArray = Math.min(array);
+exports.min = function min(array) {
+    // if (array.length === 0 ) {
+    //     return 0;
+    // } 
+    // else {
+        let minArray = Math.min.apply(null,array);
         return minArray;
-    }
+    // }
 };
 
 exports.max = function max(array) {
-    if (array.length === 0) {
-        return 0;} 
+    //  if (array.length === 0) {
+    //  return 0;} 
     // else {
-    //     let maxArray = Math.max(array);
-    //     return maxArray;
+        let maxArray = Math.max.apply(null, array);
+        return maxArray;
     // }
 };
 
 exports.avg = function avg([array]) {
-    if (array.length === 0) {
-        return 0;
-    } else {
-        let avgArray = array.reduce() / 2;
+    // if (array.length === 0) {
+    //     return 0;
+    // } else {
+        let avgArray = array.reduce((a, b)=>return a+b) / 2;
         return avgArray;
-    }
+    // }
 };
